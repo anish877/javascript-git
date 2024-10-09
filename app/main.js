2e61985d3cc40bdb96020bc6870f309cca8f3772
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-09 22:14:23 +0530

    stage 1

--- app/main.js
+++ app/main.js
@@ -5,21 +5,21 @@ const path = require("path");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-// const command = process.argv[2];
-//
-// switch (command) {
-//   case "init":
-//     createGitDirectory();
-//     break;
-//   default:
-//     throw new Error(`Unknown command ${command}`);
-// }
-//
-// function createGitDirectory() {
-//   fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
-//   fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
-//   fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
-//
-//   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
-//   console.log("Initialized git directory");
-// }
+const command = process.argv[2];
+
+switch (command) {
+  case "init":
+    createGitDirectory();
+    break;
+  default:
+    throw new Error(`Unknown command ${command}`);
+}
+
+function createGitDirectory() {
+  fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
+  fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
+
+  fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
+  console.log("Initialized git directory");
+}
